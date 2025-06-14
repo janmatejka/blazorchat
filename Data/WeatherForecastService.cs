2d1b06e60d79ab7d59c9b4be952d659ca2e650af
Jan Matějka <matejka@datasys.cz>
2025-06-14 13:27:32 +0200

    forecast mění data

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -1,19 +1,43 @@
 namespace blazorchat3.Data;
 
+using Timer = System.Threading.Timer;
+
 public class WeatherForecastService
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
-    public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+    
+    private readonly Timer _timer;
+    private WeatherForecast[] _forecasts;
+    public event Action? OnForecastChanged;
+    
+    public WeatherForecastService()
     {
-        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        _forecasts = GenerateForecasts(DateOnly.FromDateTime(DateTime.Now));
+        // Aktualizace každých 2 sekund
+        _timer = new Timer(_ => UpdateForecasts(), null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+    }
+    
+    private void UpdateForecasts()
+    {
+        _forecasts = GenerateForecasts(DateOnly.FromDateTime(DateTime.Now));
+        OnForecastChanged?.Invoke();
+    }
+    
+    private WeatherForecast[] GenerateForecasts(DateOnly startDate)
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = startDate.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        }).ToArray());
+        }).ToArray();
+    }
+
+    public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+    {
+        return Task.FromResult(_forecasts);
     }
 }
